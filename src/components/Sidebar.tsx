6d947740c1d5ec2798e7b616acfbc93462eaea15
davidivad96 <davidivad96@gmail.com>
2024-07-20 18:13:18 +0200

    display text when there are no stories yet

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 import { Story } from "@/types";
 import { getKvKey } from "@/utils";
-import { Card, Flex, Heading, ScrollArea } from "@radix-ui/themes";
+import { Card, Flex, Heading, ScrollArea, Text } from "@radix-ui/themes";
 import { kv } from "@vercel/kv";
 import StoryCard from "./StoryCard";
 
@@ -10,15 +10,22 @@ const Sidebar: React.FC = async () => {
   const stories = await kv.hgetall<Record<string, Story>>(
     getKvKey(session?.user?.email ?? "guest")
   );
+  const storiesArray = Object.values(stories ?? []);
 
   return (
     <Card className="p-4 w-72" style={{ height: "calc(100vh - 166px)" }}>
       <Flex direction="column" height="100%">
         <Heading mb="4">{session ? "Your Stories" : "Guest Stories"}</Heading>
         <ScrollArea className="h-full">
-          {Object.values(stories ?? []).map((story) => (
-            <StoryCard key={story.id} story={story} canDelete={!!session} />
-          ))}
+          {storiesArray.length > 0 ? (
+            Object.values(storiesArray).map((story) => (
+              <StoryCard key={story.id} story={story} canDelete={!!session} />
+            ))
+          ) : (
+            <Text className="text-gray-500 text-center mt-4">
+              No stories yet
+            </Text>
+          )}
         </ScrollArea>
       </Flex>
     </Card>
